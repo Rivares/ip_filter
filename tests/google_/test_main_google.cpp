ae795303d1d526a97e6d0c34617ec1787f3f4110
Rivares <ura1994nikolaevic@gmail.com>
2023-09-10 23:26:30 +0300

    Decided homework #2

---
+++ tests/google_/test_main_google.cpp
@@ -0,0 +1,12 @@
+#define GOOGLE_TEST_MODULE test_main_google
+
+#include "test_main_google.hpp"
+
+#include <gtest/gtest.h>
+
+
+
+TEST(TestGroupName, Subtest_1)
+{
+    //ASSERT_TRUE(version() != 100);
+}
