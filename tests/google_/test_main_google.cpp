932a6f87ec2089e43ace2619906899369cfba36a
Rivares <ura1994nikolaevic@gmail.com>
2023-09-11 01:42:38 +0300

    Decided homework #2

--- tests/google_/test_main_google.cpp
+++ tests/google_/test_main_google.cpp
@@ -4,9 +4,52 @@
 
 #include <gtest/gtest.h>
 
-
+#include <charconv>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <cstdlib>
+#include <string>
+#include <array>
+#include <set>
 
 TEST(TestGroupName, Subtest_1)
 {
-    //ASSERT_TRUE(version() != 100);
+    std::ifstream file("ip_filter.tsv");
+    if (file.is_open())
+    {
+        std::multiset<std::array<int, cntDigits>, ModeComp> ip_pool;
+
+        std::string buff = "";
+        while (std::getline(file, buff))
+        {
+            std::array<int, cntDigits> ip_add {0, 0, 0, 0};
+            std::string_view ip_addr = buff.substr(0, buff.find_first_of('\t'));
+
+            std::string_view ip_part = ip_addr.substr(0, ip_addr.find_first_of('.'));
+            std::from_chars(ip_part.data(), ip_part.data() + ip_part.size(), ip_add[0]);
+            ip_addr.remove_prefix(ip_addr.find_first_of('.') + 1);
+
+            ip_part = ip_addr.substr(0, ip_addr.find_first_of('.'));
+            std::from_chars(ip_part.data(), ip_part.data() + ip_part.size(), ip_add[1]);
+            ip_addr.remove_prefix(ip_addr.find_first_of('.') + 1);
+
+            ip_part = ip_addr.substr(0, ip_addr.find_first_of('.'));
+            std::from_chars(ip_part.data(), ip_part.data() + ip_part.size(), ip_add[2]);
+            ip_addr.remove_prefix(ip_addr.find_first_of('.') + 1);
+
+            ip_part = ip_addr.substr(0, ip_addr.find_first_of('.'));
+            std::from_chars(ip_part.data(), ip_part.data() + ip_part.size(), ip_add[3]);
+
+            ip_pool.insert(ip_add);
+        }
+
+        // Reverse lexicographically sort
+        for (auto addr = ip_pool.begin(); addr != ip_pool.end(); ++addr)
+        {
+            ASSERT_TRUE((*addr).at(0) >= (*(++addr)).at(0));
+        }
+    }
+    else
+    {  ASSERT_TRUE(false); }
 }
