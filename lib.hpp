9d0cee3653f2ee1e5b602f5ced9bd40e37c4835e
Rivares <ura1994nikolaevic@gmail.com>
2023-09-11 00:01:57 +0300

    Decidedhomework #2

--- lib.hpp
+++ lib.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <charconv>
 #include <iostream>
+#include <fstream>
 #include <cassert>
 #include <cstdlib>
 #include <string>
 #include <array>
-#include <charconv>
 #include <set>
 
 static constexpr size_t cntDigits = 4;
@@ -48,84 +49,88 @@ struct ModeComp
 
 void pluginAutoLambda()
 {
-    try
+    std::ifstream file("ip_filter.tsv");
+    if (file.is_open())
     {
-        std::multiset<std::array<int, cntDigits>, ModeComp> ip_pool;
-
-        std::string buff = "";
-        while (std::getline(std::cin, buff))
+        try
         {
-            std::array<int, cntDigits> ip_add {0, 0, 0, 0};
-            std::string_view ip_addr = buff.substr(0, buff.find_first_of('\t'));
+            std::multiset<std::array<int, cntDigits>, ModeComp> ip_pool;
 
-            std::string_view ip_part = ip_addr.substr(0, ip_addr.find_first_of('.'));
-            std::from_chars(ip_part.data(), ip_part.data() + ip_part.size(), ip_add[0]);
-            ip_addr.remove_prefix(ip_addr.find_first_of('.') + 1);
+            std::string buff = "";
+            while (std::getline(file, buff))
+            {
+                std::array<int, cntDigits> ip_add {0, 0, 0, 0};
+                std::string_view ip_addr = buff.substr(0, buff.find_first_of('\t'));
 
-            ip_part = ip_addr.substr(0, ip_addr.find_first_of('.'));
-            std::from_chars(ip_part.data(), ip_part.data() + ip_part.size(), ip_add[1]);
-            ip_addr.remove_prefix(ip_addr.find_first_of('.') + 1);
+                std::string_view ip_part = ip_addr.substr(0, ip_addr.find_first_of('.'));
+                std::from_chars(ip_part.data(), ip_part.data() + ip_part.size(), ip_add[0]);
+                ip_addr.remove_prefix(ip_addr.find_first_of('.') + 1);
 
-            ip_part = ip_addr.substr(0, ip_addr.find_first_of('.'));
-            std::from_chars(ip_part.data(), ip_part.data() + ip_part.size(), ip_add[2]);
-            ip_addr.remove_prefix(ip_addr.find_first_of('.') + 1);
+                ip_part = ip_addr.substr(0, ip_addr.find_first_of('.'));
+                std::from_chars(ip_part.data(), ip_part.data() + ip_part.size(), ip_add[1]);
+                ip_addr.remove_prefix(ip_addr.find_first_of('.') + 1);
 
-            ip_part = ip_addr.substr(0, ip_addr.find_first_of('.'));
-            std::from_chars(ip_part.data(), ip_part.data() + ip_part.size(), ip_add[3]);
+                ip_part = ip_addr.substr(0, ip_addr.find_first_of('.'));
+                std::from_chars(ip_part.data(), ip_part.data() + ip_part.size(), ip_add[2]);
+                ip_addr.remove_prefix(ip_addr.find_first_of('.') + 1);
 
-            ip_pool.insert(ip_add);
+                ip_part = ip_addr.substr(0, ip_addr.find_first_of('.'));
+                std::from_chars(ip_part.data(), ip_part.data() + ip_part.size(), ip_add[3]);
+
+                ip_pool.insert(ip_add);
+            }
+
+            // Reverse lexicographically sort
+            for (const auto& addr : ip_pool)
+            {
+                std::cout << addr.at(0) << '.'
+                          << addr.at(1) << '.'
+                          << addr.at(2) << '.'
+                          << addr.at(3) << '\n';
+            }
+
+            // Filter by first byte and output . ip = filter(1)
+            for (const auto& addr : ip_pool)
+            {
+                if (addr.at(0) != 1) { continue; }
+
+                std::cout << addr.at(0) << '.'
+                          << addr.at(1) << '.'
+                          << addr.at(2) << '.'
+                          << addr.at(3) << '\n';
+            }
+
+            // Filter by first and second bytes and output . ip = filter(46, 70)
+            for (const auto& addr : ip_pool)
+            {
+                if ( (addr.at(0) != 46) || (addr.at(1) != 70) ) { continue; }
+
+                std::cout << addr.at(0) << '.'
+                          << addr.at(1) << '.'
+                          << addr.at(2) << '.'
+                          << addr.at(3) << '\n';
+            }
+
+            // Filter by any byte and output . ip = filter_any(46)
+            for (const auto& addr : ip_pool)
+            {
+                if (
+                        (addr.at(0) != 46)
+                         && (addr.at(1) != 46)
+                         && (addr.at(2) != 46)
+                         && (addr.at(3) != 46)
+                     ) { continue; }
+
+                std::cout << addr.at(0) << '.'
+                          << addr.at(1) << '.'
+                          << addr.at(2) << '.'
+                          << addr.at(3) << '\n';
+            }
         }
-
-        // Reverse lexicographically sort
-        for (const auto& addr : ip_pool)
+        catch (const std::exception& except)
         {
-            std::cout << addr.at(0) << '.'
-                      << addr.at(1) << '.'
-                      << addr.at(2) << '.'
-                      << addr.at(3) << '\n';
+            std::cerr << except.what() << std::endl;
         }
-
-        // Filter by first byte and output . ip = filter(1)
-        for (const auto& addr : ip_pool)
-        {
-            if (addr.at(0) != 1) { continue; }
-
-            std::cout << addr.at(0) << '.'
-                      << addr.at(1) << '.'
-                      << addr.at(2) << '.'
-                      << addr.at(3) << '\n';
-        }
-
-        // Filter by first and second bytes and output . ip = filter(46, 70)
-        for (const auto& addr : ip_pool)
-        {
-            if ( (addr.at(0) != 46) || (addr.at(1) != 70) ) { continue; }
-
-            std::cout << addr.at(0) << '.'
-                      << addr.at(1) << '.'
-                      << addr.at(2) << '.'
-                      << addr.at(3) << '\n';
-        }
-
-        // Filter by any byte and output . ip = filter_any(46)
-        for (const auto& addr : ip_pool)
-        {
-            if (
-                    (addr.at(0) != 46)
-                     && (addr.at(1) != 46)
-                     && (addr.at(2) != 46)
-                     && (addr.at(3) != 46)
-                 ) { continue; }
-
-            std::cout << addr.at(0) << '.'
-                      << addr.at(1) << '.'
-                      << addr.at(2) << '.'
-                      << addr.at(3) << '\n';
-        }
-    }
-    catch (const std::exception& except)
-    {
-        std::cerr << except.what() << std::endl;
     }
 }
 
